4401e3a577a72577a213db6fa40b4d593de06a7c
Vitor Villar <vitor.luis98@gmail.com>
2023-01-22 00:10:57 +0100

    Added first unit test

--- src/test/java/gg/bayes/dota_challenge/application/CreateMatchServiceTest.java
+++ src/test/java/gg/bayes/dota_challenge/application/CreateMatchServiceTest.java
@@ -1,32 +1,69 @@
 package gg.bayes.dota_challenge.application;
 
-import gg.bayes.dota_challenge.domain.Match;
+import gg.bayes.dota_challenge.adapter.repository.CombatLogEntryRepository;
+import gg.bayes.dota_challenge.adapter.repository.MatchRepository;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.AdditionalAnswers;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.util.ResourceUtils;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class CreateMatchServiceTest {
+    @Mock
+    private MatchRepository matchRepository;
+
+    @Mock
+    private CombatLogEntryRepository combatLogEntryRepository;
+
+    @Mock
+    private PlatformTransactionManager platformTransactionManager;
+
+    private CreateMatchService createMatchService;
+
+    private static Stream<Arguments> testFiles() {
+        return Stream.of(
+            Arguments.of("classpath:combatlog_1.log.txt", 4237),
+            Arguments.of("classpath:combatlog_2.log.txt", 4407)
+        );
+    }
 
     @BeforeEach
     void setUp() {
+        createMatchService = new CreateMatchService(
+            matchRepository,
+            combatLogEntryRepository,
+            platformTransactionManager
+        );
     }
 
-    @Test
-    void testCreate() throws IOException {
-        var file = ResourceUtils.getFile("classpath:combatlog_1.log.txt");
-        var content = new HashSet<>(Files.readAllLines(file.toPath()));
+    @ParameterizedTest
+    @MethodSource(value = "testFiles")
+    void testCreateFullMatch(String testFile, int combatEntries) throws IOException {
+        var file = ResourceUtils.getFile(testFile);
+        var entries = Files.readAllLines(file.toPath());
 
-        var m = Match.create(content);
+        when(matchRepository.save(any())).then(AdditionalAnswers.returnsFirstArg());
+
+        var match = createMatchService.createMatch(entries);
+
+        assertThat(match).isNotNull();
+        assertThat(match.getCombatLogEntries()).isNotEmpty().hasSize(combatEntries);
 
 
-        System.out.println(content);
+        System.out.println(entries);
     }
 }
